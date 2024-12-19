6750c1618666f6973a716d1c6ccc95f62f4f0e6e
Miss-Nikita <bisennikita770@gmail.com>
2024-12-19 12:23:42 +0530

    Start frontend

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,35 +1,59 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// src/App.js
+import { Route, Routes } from "react-router-dom";
+import ProtectedRoute from "./components/auth/ProtectedRoute";
+import Home from "./pages/Home";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+import Dashboard from "./pages/Dashboard";
 
-function App() {
-  const [count, setCount] = useState(0)
+import { useEffect } from "react";
+import SingleProperty from "./pages/SingleProperty";
+import Nav from "./pages/partials/Nav";
+import BookingPage from "./pages/BookingPage";
+import ProfilePage from "./pages/ProfilePage";
+import EditProperty from "./pages/EditProperty";
+import CreateProperty from "./pages/CreateProperty";
+import AdminPanel from "./pages/AdminPanel";
+import AllUser from "./pages/partials/AllUser";
+import AllPayment from "./pages/partials/AllPayment";
+import Allproperties from "./pages/partials/AllProperties";
+import AllBookings from "./pages/partials/AllBookings";
+
+const App = () => {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
+      <Nav />
 
-export default App
+      <Routes>
+      <Route path="/admin-panel" element={<AdminPanel />}>
+          {/* Child Route */}
+          <Route path="users" element={<AllUser />} />
+          <Route path="properties" element={<Allproperties />} />
+          <Route path="bookings" element={<AllBookings />} />
+          <Route path="payments" element={<AllPayment />} />
+        </Route>
+
+        <Route path="/" element={<Home />} />
+        <Route path="/property/create/" element={<CreateProperty />} />
+        <Route path="/property/edit/:id" element={<EditProperty />} />
+        <Route path="/property/:id" element={<SingleProperty />} />
+        <Route path="/Booking/:id" element={<BookingPage />} />
+        <Route path="/profile/" element={<ProfilePage />} />
+
+        <Route path="/login" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+        <Route
+          path="/dashboard"
+          element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default App;
--- Frontend/src/main.jsx
+++ Frontend/src/main.jsx
@@ -1,10 +1,17 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import {store} from "./store/store.jsx";
+import { Provider } from "react-redux";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+createRoot(document.getElementById("root")).render(
+    <Provider store={store}>
+        <BrowserRouter>
+            <App />
+            <ToastContainer />
+        </BrowserRouter>
+    </Provider>
+);
